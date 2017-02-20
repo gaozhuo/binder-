5bdda55626d5be78bf792423aadcf4015c43df56
gaozhuo <1802928215@qq.com>
2017-02-20 22:48:12 +0800

    commit

--- Binder.c
+++ Binder.c
@@ -3,7 +3,7 @@
 */
 static int binder_open(struct inode *nodp, struct file *filp)
 {
-    struct binder_proc *proc; // binder进程 【见附录3.1】
+    struct binder_proc *proc; // 表示进程
 
     proc = kzalloc(sizeof(*proc), GFP_KERNEL); // 为binder_proc分配内存空间
    
@@ -25,6 +25,7 @@ struct binder_proc {
 	struct rb_root threads;//binder_thread红黑树的根节点，表示进程中的binder线程
 
 	struct rb_root nodes;//binder_node红黑树的根节点
+
 	struct rb_root refs_by_desc;//binder_ref红黑树的根节点(以handle为key)
 	struct rb_root refs_by_node;//binder_ref红黑树的根节点（以binder_node为key）
 
@@ -32,6 +33,18 @@ struct binder_proc {
 	ptrdiff_t user_buffer_offset;//内核空间与用户空间的地址偏移量
 	size_t buffer_size;//内核缓存区大小
 	
+	struct list_head todo;//todo队列，即该进程需要处理的任务
+	wait_queue_head_t wait;//等待队列，进程空闲时在此队列上休眠
+};
+
+/**
+*内核中表示线程的结构体
+*/
+struct binder_thread {
+	struct binder_proc *proc;//所属进程
+
+	struct binder_transaction *transaction_stack;//正在处理的事务
+
 	struct list_head todo;//todo队列，即该进程需要处理的任务
 	wait_queue_head_t wait;//等待队列，进程空闲时在此队列上休眠
 };
